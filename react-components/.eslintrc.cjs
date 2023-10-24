e92852f930a2bb6dc75b426da43b652b2592b96c
Valery Matskevich <v.matskevich93@gmail.com>
2023-10-24 19:14:40 +0300

    feat: add and configure prettier

--- react-components/.eslintrc.cjs
+++ react-components/.eslintrc.cjs
@@ -9,20 +9,15 @@ module.exports = {
     'airbnb/hooks',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  overrides: [
-  ],
+  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './react-components/tsconfig.json',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-    'react/react-in-jsx-scope': 0,
-  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: { 'react/react-in-jsx-scope': 0 },
 };
