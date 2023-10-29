bcd30bc937201c9869c189c46382b8b1531e0a0d
Valery Matskevich <v.matskevich93@gmail.com>
2023-10-30 01:02:54 +0300

    fix: change the returned component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
+import PokemonsPage from './components/PokemonsPage';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <PokemonsPage />;
 }
 
 export default App;
