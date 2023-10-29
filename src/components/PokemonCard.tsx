846782d9ad3b128afab26fcc5fcc19e37b1f8265
Valery Matskevich <v.matskevich93@gmail.com>
2023-10-30 00:58:41 +0300

    feat: add PokemonCard component

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import upperCaseFirst from '../utils/upperCaseFirst';
+import classes from './PokemonCard.module.css';
+import { PokemonCardProps } from '../interface/PokemonCardProps';
+
+export default class PokemonCard extends Component<PokemonCardProps> {
+  render() {
+    const { name, img } = this.props;
+    return (
+      <div className={classes.card}>
+        <p className={classes.name}>{upperCaseFirst(name)}</p>
+        <img className={classes.img} src={img} alt={name} />
+      </div>
+    );
+  }
+}
