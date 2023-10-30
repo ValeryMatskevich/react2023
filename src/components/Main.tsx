25c83cb59fe05258c81fcb58933bb09b5893218a
Valery Matskevich <v.matskevich93@gmail.com>
2023-10-30 19:52:02 +0300

    refactor: add styles for Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,14 @@
 import { Component } from 'react';
 import PokemonsList from './PokemonsList';
 import { MainProps } from '../interface/MainProps';
+import classes from './Main.module.css';
 
 export default class Main extends Component<MainProps> {
   render() {
     const { data } = this.props;
 
     return (
-      <main>
+      <main className={classes.main}>
         <PokemonsList data={data} />
       </main>
     );
